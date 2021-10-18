0b7657c4617e3ffa5ec71659a62ca6ddb18d4aaf
DakotaJ138 <dakotaj138@gmail.com>
2021-10-18 14:51:11 -0500

    Entities Update
    Updated entities

--- src/main/java/com/cooksys/teamOneSocialMedia/entities/Hashtag.java
+++ src/main/java/com/cooksys/teamOneSocialMedia/entities/Hashtag.java
@@ -1,13 +1,30 @@
 package com.cooksys.teamOneSocialMedia.entities;
 
 import javax.persistence.Entity;
-
+import lombok.NoArgsConstructor;
+import lombok.Data;
+import java.sql.Timestamp;
+import java.util.List;
+import javax.persistence.GeneratedValue;
+import javax.persistence.Id;
+import javax.persistence.JoinColumn;
+import javax.persistence.ManyToOne;
+import javax.persistence.OneToMany;
+import javax.persistence.ManyToMany;
 import lombok.Data;
 import lombok.NoArgsConstructor;
+//import com.cooksys.teamOneSocialMedia.entities.Tweet;
 
 @Entity
 @NoArgsConstructor
 @Data
 public class Hashtag {
-
+	@Id
+	@GeneratedValue
+	private Long id;
+	private String label;
+	private Timestamp firstUsed;
+	private Timestamp lastUsed;
+	@ManyToMany(mappedBy = "hashtags")
+	private List<Tweet> tweets;
 }
--- src/main/java/com/cooksys/teamOneSocialMedia/entities/Tweet.java
+++ src/main/java/com/cooksys/teamOneSocialMedia/entities/Tweet.java
@@ -1,13 +1,41 @@
 package com.cooksys.teamOneSocialMedia.entities;
 
-import javax.persistence.Entity;
+import javax.persistence.*;
+
+import com.sun.istack.Nullable;
+import lombok.NoArgsConstructor;
+import lombok.Data;
+import java.sql.Timestamp;
+import java.util.List;
 
 import lombok.Data;
 import lombok.NoArgsConstructor;
+
+import lombok.Data;
+import lombok.NoArgsConstructor;
+
 
 @Entity
 @NoArgsConstructor
 @Data
 public class Tweet {
+    @Id
+    @GeneratedValue(strategy = GenerationType.AUTO)
+    @Column(name = "id", nullable = false)
+    private Long id;
 
+    @ManyToOne
+    @JoinColumn(name = "author_id")
+    private User author;
+    private Timestamp posted;
+    @Nullable
+    private String content;
+    @ManyToOne
+    @Nullable
+    @JoinColumn(name = "in_reply_to_ID")
+    private Tweet inReplyTo;
+    @ManyToOne
+    @Nullable
+    @JoinColumn(name = "repost_of_ID")
+    private Tweet repostOf;
 }
