42a33fd4a3a19f0cd8ec17a879d90232ae6fbc8c
Seth Klumph <stklumph@crimson.ua.edu>
2021-10-18 12:34:25 -0500

    created controllers

---
+++ src/main/java/com/cooksys/teamOneSocialMedia/controllers/UserController.java
@@ -0,0 +1,13 @@
+package com.cooksys.teamOneSocialMedia.controllers;
+
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+import lombok.RequiredArgsConstructor;
+
+@RestController
+@RequiredArgsConstructor
+@RequestMapping("/user")
+public class UserController {
+
+}
