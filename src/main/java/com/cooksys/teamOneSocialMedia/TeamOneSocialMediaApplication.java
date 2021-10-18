8f9d1f6ed96feea81d0267b585b2b0389fb57395
DakotaJ138 <dakotaj138@gmail.com>
2021-10-18 11:50:35 -0500

    Spring initializer
    Generated Project

---
+++ src/main/java/com/cooksys/teamOneSocialMedia/TeamOneSocialMediaApplication.java
@@ -0,0 +1,13 @@
+package com.cooksys.teamOneSocialMedia;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class TeamOneSocialMediaApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(TeamOneSocialMediaApplication.class, args);
+	}
+
+}
