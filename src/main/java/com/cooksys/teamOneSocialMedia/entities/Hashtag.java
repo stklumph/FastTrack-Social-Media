bd4f64a7a6d5cdeeea2ef66f4f976b7df02df835
Seth Klumph <stklumph@crimson.ua.edu>
2021-10-18 15:35:38 -0500

    Removed unnecessary imports from Tweet and Hashtag Entities

--- src/main/java/com/cooksys/teamOneSocialMedia/entities/Hashtag.java
+++ src/main/java/com/cooksys/teamOneSocialMedia/entities/Hashtag.java
@@ -1,19 +1,15 @@
 package com.cooksys.teamOneSocialMedia.entities;
 
-import javax.persistence.Entity;
-import lombok.NoArgsConstructor;
-import lombok.Data;
 import java.sql.Timestamp;
 import java.util.List;
+
+import javax.persistence.Entity;
 import javax.persistence.GeneratedValue;
 import javax.persistence.Id;
-import javax.persistence.JoinColumn;
-import javax.persistence.ManyToOne;
-import javax.persistence.OneToMany;
 import javax.persistence.ManyToMany;
+
 import lombok.Data;
 import lombok.NoArgsConstructor;
-//import com.cooksys.teamOneSocialMedia.entities.Tweet;
 
 @Entity
 @NoArgsConstructor
@@ -22,9 +18,13 @@ public class Hashtag {
 	@Id
 	@GeneratedValue
 	private Long id;
+
 	private String label;
+
 	private Timestamp firstUsed;
+
 	private Timestamp lastUsed;
+
 	@ManyToMany(mappedBy = "hashtags")
 	private List<Tweet> tweets;
 }
--- src/main/java/com/cooksys/teamOneSocialMedia/entities/Tweet.java
+++ src/main/java/com/cooksys/teamOneSocialMedia/entities/Tweet.java
@@ -1,41 +1,45 @@
 package com.cooksys.teamOneSocialMedia.entities;
 
-import javax.persistence.*;
+import java.sql.Timestamp;
+
+import javax.persistence.Column;
+import javax.persistence.Entity;
+import javax.persistence.GeneratedValue;
+import javax.persistence.GenerationType;
+import javax.persistence.Id;
+import javax.persistence.JoinColumn;
+import javax.persistence.ManyToOne;
 
 import com.sun.istack.Nullable;
-import lombok.NoArgsConstructor;
-import lombok.Data;
-import java.sql.Timestamp;
-import java.util.List;
 
 import lombok.Data;
 import lombok.NoArgsConstructor;
-
-import lombok.Data;
-import lombok.NoArgsConstructor;
-
 
 @Entity
 @NoArgsConstructor
 @Data
 public class Tweet {
-    @Id
-    @GeneratedValue(strategy = GenerationType.AUTO)
-    @Column(name = "id", nullable = false)
-    private Long id;
+	@Id
+	@GeneratedValue(strategy = GenerationType.AUTO)
+	@Column(name = "id", nullable = false)
+	private Long id;
 
-    @ManyToOne
-    @JoinColumn(name = "author_id")
-    private User author;
-    private Timestamp posted;
-    @Nullable
-    private String content;
-    @ManyToOne
-    @Nullable
-    @JoinColumn(name = "in_reply_to_ID")
-    private Tweet inReplyTo;
-    @ManyToOne
-    @Nullable
-    @JoinColumn(name = "repost_of_ID")
-    private Tweet repostOf;
+	@ManyToOne
+	@JoinColumn(name = "author_id")
+	private User author;
+
+	private Timestamp posted;
+
+	@Nullable
+	private String content;
+
+	@ManyToOne
+	@Nullable
+	@JoinColumn(name = "in_reply_to_ID")
+	private Tweet inReplyTo;
+
+	@ManyToOne
+	@Nullable
+	@JoinColumn(name = "repost_of_ID")
+	private Tweet repostOf;
 }
