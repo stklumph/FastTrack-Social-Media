94a9138c028ce93fc25fbdafe9a2a191d886e620
Seth Klumph <stklumph@crimson.ua.edu>
2021-10-18 15:33:43 -0500

    Restructured Controller Advice

---
+++ src/main/java/com/cooksys/teamOneSocialMedia/controllers/advice/SocialMediaControllerAdvice.java
@@ -0,0 +1,38 @@
+package com.cooksys.teamOneSocialMedia.controllers.advice;
+
+import javax.servlet.http.HttpServletRequest;
+
+import org.springframework.http.HttpStatus;
+import org.springframework.web.bind.annotation.ControllerAdvice;
+import org.springframework.web.bind.annotation.ExceptionHandler;
+import org.springframework.web.bind.annotation.ResponseBody;
+import org.springframework.web.bind.annotation.ResponseStatus;
+
+import com.cooksys.teamOneSocialMedia.dtos.ErrorDto;
+import com.cooksys.teamOneSocialMedia.exceptions.BadRequestException;
+import com.cooksys.teamOneSocialMedia.exceptions.NotAuthorizedException;
+import com.cooksys.teamOneSocialMedia.exceptions.NotFoundException;
+
+@ControllerAdvice(basePackages = { "com.cooksys.teamOneSocialMedia.controllers" })
+@ResponseBody
+public class SocialMediaControllerAdvice {
+
+	@ResponseStatus(HttpStatus.BAD_REQUEST)
+	@ExceptionHandler(BadRequestException.class)
+	public ErrorDto handleBadRequestException(HttpServletRequest request, BadRequestException badRequestException) {
+		return new ErrorDto(badRequestException.getMessage());
+	}
+
+	@ResponseStatus(HttpStatus.NOT_FOUND)
+	@ExceptionHandler(NotFoundException.class)
+	public ErrorDto handleNotFoundException(HttpServletRequest request, NotFoundException notFoundException) {
+		return new ErrorDto(notFoundException.getMessage());
+	}
+
+	@ResponseStatus(HttpStatus.NOT_FOUND)
+	@ExceptionHandler(NotFoundException.class)
+	public ErrorDto handleNotAuthorizedException(HttpServletRequest request,
+			NotAuthorizedException notAuthorizedException) {
+		return new ErrorDto(notAuthorizedException.getMessage());
+	}
+}
