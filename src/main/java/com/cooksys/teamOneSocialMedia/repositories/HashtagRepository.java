1e634119dc58aa93213ac7e1eba620a0da1a6adb
Seth Klumph <stklumph@crimson.ua.edu>
2021-10-19 10:12:01 -0500

    added findById to Repositories

--- src/main/java/com/cooksys/teamOneSocialMedia/repositories/HashtagRepository.java
+++ src/main/java/com/cooksys/teamOneSocialMedia/repositories/HashtagRepository.java
@@ -1,12 +1,13 @@
 package com.cooksys.teamOneSocialMedia.repositories;
 
+import java.util.Optional;
+
 import org.springframework.data.jpa.repository.JpaRepository;
 import org.springframework.stereotype.Repository;
 
 import com.cooksys.teamOneSocialMedia.entities.Hashtag;
 
 @Repository
-public interface HashtagRepository extends JpaRepository<Hashtag, Long> { // FIXME: update the id type if a Long is not
-																			// used for the ID of the hashtags
-
+public interface HashtagRepository extends JpaRepository<Hashtag, Long> {
+	Optional<Hashtag> findById(Long id);
 }
--- src/main/java/com/cooksys/teamOneSocialMedia/repositories/UserRepository.java
+++ src/main/java/com/cooksys/teamOneSocialMedia/repositories/UserRepository.java
@@ -1,5 +1,7 @@
 package com.cooksys.teamOneSocialMedia.repositories;
 
+import java.util.Optional;
+
 import org.springframework.data.jpa.repository.JpaRepository;
 import org.springframework.stereotype.Repository;
 
@@ -7,5 +9,5 @@ import com.cooksys.teamOneSocialMedia.entities.User;
 
 @Repository
 public interface UserRepository extends JpaRepository<User, Long> {
-
+	Optional<User> findByIdAndDeletedFalse(Long id);
 }
