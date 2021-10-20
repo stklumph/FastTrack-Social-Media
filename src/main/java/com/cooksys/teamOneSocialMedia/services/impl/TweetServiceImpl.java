79fa2dea5379d44c618b9b18e9b9ca63d90d3a11
Stephen Sohns <37886718+scsohns@users.noreply.github.com>
2021-10-20 15:49:13 -0500

    removed unused import

--- src/main/java/com/cooksys/teamOneSocialMedia/services/impl/TweetServiceImpl.java
+++ src/main/java/com/cooksys/teamOneSocialMedia/services/impl/TweetServiceImpl.java
@@ -2,7 +2,6 @@ package com.cooksys.teamOneSocialMedia.services.impl;
 
 import java.util.ArrayList;
 import java.util.Collections;
-import java.util.Comparator;
 import java.util.List;
 import java.util.Optional;
 import java.util.Stack;
